adf62035141c121df7a20e29299429ad90dfc47a
M <584706394@qq.com>
2019-08-14 17:27:31 +0800

    第三次提交

--- src/pages/cart/index.vue
+++ src/pages/cart/index.vue
@@ -1,29 +1,336 @@
 <template>
-  <div>
-
-
+  <div class="cart">
+    <div class="cart-container">
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item">
+        <div class="item-left circle"></div>
+        <div class="item-img">
+          <img src="../../../static/images/img1.jpg" alt />
+        </div>
+        <div class="item-right">
+          <p class="right-name">招牌麻辣鱼</p>
+          <p class="right-price">￥98/份</p>
+          <div class="right-count1">
+            <span class="count-reduce">-</span>
+            <span class="count-number">10</span>
+            <span class="count-add">+</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="cart-bottom">
+      <div class="bottom-left circle"></div>
+      <p class="p1">
+        <span class="span1">全选</span>
+        <span class="span2">合计：</span>
+        <span class="span3">￥98元</span>
+      </p>
+      <div class="bottom-btn">结算</div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
 
-  components: {
-  },
+  components: {},
 
-  methods: {
-  
-  },
+  methods: {},
 
-  created () {
+  created() {
     // let app = getApp()
   }
-}
+};
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.circle{
+
+      width: 44rpx;
+      height: 44rpx;
+      border: 1rpx solid #000;
+      border-radius: 50%;
+      background-color: #fff;
+}
+.cart{
+  padding: 29rpx 22rpx 107rpx 29rpx;
+  .cart-container{
+    box-shadow: 2px 2px 6px #909090;
+    border-radius: 10rpx;
+    padding: 11rpx 27rpx 11rpx 19rpx;
+
+    .cart-item{
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-bottom: 10rpx;
+    height: 95rpx;
+      .item-left{
+        
+      }
+      .item-img{
+        margin-left:47rpx;
+        margin-right: 18rpx;
+          height: 95rpx;
+          width: 95rpx;
+        img{
+          height: 95rpx;
+          width: 95rpx;
+        }
+      }
+      .item-right{
+        flex:1;
+        position: relative;
+        font-size: 26rpx;
+        .right-name{
+          color:rgb(188,188,188);
+          margin-bottom: 30rpx;
+        }
+        .right-price{
+          color:rgb(255,72,0);}
+        .right-count1{
+          position: absolute;
+          right: 0;
+          bottom:10rpx;
+        }
+      }
+    }
+    .cart-item:last-child{
+      margin-bottom: 0;
+    }
+  }
+  .cart-bottom{
+    width: 100%;
+    position: fixed;
+    bottom:0;
+    left:0;
+    height: 85rpx;
+    background-color: rgb(249,217,41);
+    color:#000;
+    line-height: 85rpx;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0 49rpx 0 44rpx;
+    font-size: 26rpx;
+    box-sizing: border-box;
+    .bottom-left{
+      flex:0 0 44rpx;
+    }
+    .p1{
+      flex:1;
+      text-align: right;
+      padding: 0 50rpx 0 5rpx;
+      .span1{
+
+        margin-right: 200rpx;
+      }
+      .span2{}
+      .span3{
+        color:rgb(255,72,0);
+      }
+    }
+    .bottom-btn{
+      flex:0 0 113rpx;
+      border: 1rpx solid #000;
+      // padding: 5rpx 5rpx;
+      height: 65rpx;
+      width: 113rpx;
+      line-height: 65rpx;
+      text-align: center;
+      background-color: #fff;
+    }
+  }
+}
+.right-count1 {
+  background-color: rgb(243, 245, 249);
+  width: 132rpx;
+  height: 30rpx;
+  line-height: 30rpx;
+  border-radius: 30rpx;
+  display: flex;
+  justify-content: space-around;
+}
 </style>
